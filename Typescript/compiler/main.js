cf2f95621cf0975287cb115af109984d0355c77a
luyujie <1686661978@qq.com>
2024-01-26 15:58:46 +0800

    wx-小程序

--- Typescript/compiler/main.js
+++ Typescript/compiler/main.js
@@ -1,43 +1,6 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-class NestObj {
-}
-class Validator {
-}
-__decorate([
-    params,
-    __metadata("design:type", NestObj)
-], Validator.prototype, "nestObj", void 0);
-function params(obj, key, index) { }
-class Test {
-    constructor() { }
-    methods(arg1, arg2) { }
-    methods2(arg1, arg2) { }
-}
-__decorate([
-    __param(0, params),
-    __param(1, params),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Validator, Number]),
-    __metadata("design:returntype", void 0)
-], Test.prototype, "methods", null);
-__decorate([
-    __param(0, params),
-    __param(1, params),
-    __metadata("design:type", Function),
-    __metadata("design:paramtypes", [Validator, Number]),
-    __metadata("design:returntype", void 0)
-], Test.prototype, "methods2", null);
-const obj = new Test();
-obj.methods({ value: "fda" }, 123);
+var PageNoEnum;
+(function (PageNoEnum) {
+    PageNoEnum["pageNo"] = "pageNo1";
+    PageNoEnum["curPage"] = "curPage2";
+})(PageNoEnum || (PageNoEnum = {}));
--- Typescript/src/main.ts
+++ Typescript/src/main.ts
@@ -1,23 +1,7 @@
-class NestObj {
-  value: number;
+interface a {
+  a: string
 }
 
-class Validator {
-  value: string;
-  @params
-  nestObj: NestObj;
+interface b extends a {
+  b: number
 }
-
-function params(obj: object, key: string, index: number) {}
-
-class Test {
-  constructor() {}
-
-  methods(@params arg1: Validator, @params arg2: number) {}
-
-  methods2(@params arg1: Validator, @params arg2: number) {}
-}
-
-const obj = new Test();
-
-obj.methods({ value: "fda" }, 123);
